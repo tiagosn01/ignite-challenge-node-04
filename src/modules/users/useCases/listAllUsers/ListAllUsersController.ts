1290cf454d6db9b0053b219a238f99cfac121b29
Tiago Silva <tiagosilva0922@gmail.com>
2021-10-12 22:28:51 -0300

    feat: exercices resolved

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,17 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    try {
+      const listUsers = this.listAllUsersUseCase.execute({
+        user_id: String(user_id),
+      });
+
+      return response.status(200).json(listUsers);
+    } catch (error) {
+      return response.status(400).json({ error: "You dont have permission" });
+    }
   }
 }
 
